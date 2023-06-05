5055dc86c5d6b70091d764a0129a92a035de9ae0
“AndreaFerreiro” <andreafga28@gmail.com>
2023-06-05 16:36:14 +0200

    try to refact

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,6 +2,12 @@ const inputName = document.querySelector('.input_name');
 const inputRef = document.querySelector('.input_ref');
 const inputCat = document.querySelector ('.input_cat');
 const inputStock = document.querySelector ('.input_stock');
+const newElement={
+    name:
+    ref:
+    categorie:
+    stock:
+}
 const answerName = document.querySelector ('.dataelement__text--title');
 const answerRef = document.querySelector ('.dataelement__text--answerref');
 const answerCat = document.querySelector ('.dataelement__text--answercat');
@@ -9,40 +15,47 @@ const answerStock = document.querySelector ('.dataelement__text--answerstock');
 const submitButtom = document.querySelector ('.articleform__submit');
 const message =document.querySelector ('.articleform__message');
 const list =document.querySelector ('.data__containerlist')
-
+function renderElement(newElement){
+    list.innerHTML+= `
+    <li class="data__containerlist--element">
+        <article class="dataelement">
+            <div class="dataelement__img">
+                <img class="dataelement__img--src" src="./assets/images/obj.jpeg"/>
+            </div>
+            <div class="dataelement__text">
+                <p class="dataelement__text--title">${newElement.name}</p>
+                <div class="dataelement__text--ref">
+                    <p class="dataelement__text--questionref">Ref:</p>
+                    <p class="dataelement__text--answerref">${newElement.ref}</p>
+                </div>
+                <div class="dataelement__text--cat">
+                    <p class="dataelement__text--questioncat">Categoría:</p>
+                    <p class="dataelement__text--answercat">${newElement.categorie}</p>
+                </div>
+                <div class="dataelement__text--stock">
+                    <p class="dataelement__text--questionstock">Stock:</p>
+                    <p class="dataelement__text--answerstock">${newElement.stock}</p>
+                </div>
+            </div>
+        </article>
+</li>`
+ console.log(renderElement());
+}
 function newElement(event){
-    const nameValue = inputName.value;
+    event.preventDefault();
+    /*const nameValue = inputName.value;
     const refValue = inputRef.value;
     const catValue = inputCat.value;
-    const stockValue = inputStock.value;
-    event.preventDefault();
-    if (nameValue === '' || refValue === '' || catValue === '' || stockValue === ''){
+    const stockValue = inputStock.value;*/
+    newElement.name= inputName.value;
+    newElement.ref = inputRef.value;
+    newElement.categorie= inputCat.value;
+    newElement.stock= inputStock.value;
+    if (newElement.name === '' || newElement.ref === '' || newElement.categorie === '' || newElement.stock === ''){
         message.innerHTML = 'Uy, parece que has olvidado algo';
     } else{
         message.classList.add ('collapsed')
-        list.innerHTML += 
-        `<li class="data__containerlist--element">
-            <article class="dataelement">
-                <div class="dataelement__img">
-                    <img class="dataelement__img--src" src="./assets/images/obj.jpeg"/>
-                </div>
-                <div class="dataelement__text">
-                    <p class="dataelement__text--title">${nameValue}</p>
-                    <div class="dataelement__text--ref">
-                        <p class="dataelement__text--questionref">Ref:</p>
-                        <p class="dataelement__text--answerref">${refValue}</p>
-                    </div>
-                    <div class="dataelement__text--cat">
-                        <p class="dataelement__text--questioncat">Categoría:</p>
-                        <p class="dataelement__text--answercat">${catValue}</p>
-                    </div>
-                    <div class="dataelement__text--stock">
-                        <p class="dataelement__text--questionstock">Stock:</p>
-                        <p class="dataelement__text--answerstock">${stockValue}</p>
-                    </div>
-                </div>
-            </article>
-        </li>`
+        renderElement(newElement);
     }
 }
 if( submitButtom !== null ) {
